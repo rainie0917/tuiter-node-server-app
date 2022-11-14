9074efdc706bb9081bc81cbfc0ba607f894894e7
Rainie Zhao <rainiezhao@Rainies-MacBook-Pro-2.local>
2022-11-14 13:26:59 -0800

    added tuits in tutis.js

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,7 +1,7 @@
 export default [
     {
      "_id": 123, "topic": "Space", "username": "SpaceX",
-     "handle": "spacex", "time": "2h", "image": "spacex.png",
+     "handle": "spacex", "time": "2h", "image": "spacex.jpeg",
      "title": "SpaceX's Mission",
      "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
      "liked": true, "likes": 2345,
@@ -9,13 +9,13 @@ export default [
      "replies": 123, "retuits": 432
     },
     {
-     "_id": 234, "topic": "Traffic", "username": "The Boring Company",
-     "handle": "boringcompany", "time": "2h", "image": "boring2.jpg",
-     "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
-     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-     "liked": true, "likes": 2345,
-     "disliked": false, "dislikes": 0,
-     "replies": 123, "retuits": 432
+     "_id": 345, "topic": "Energy",  "userName": "Tesla",
+     "title": "Tesla FSD finally makes driving obsolete. After years of delay, Tesla Full Self Driving can drive you to work while you work, play, or sleep",
+     "time": "4h", "image": "tesla.jpeg",
+     "liked": false, "disliked": false,
+     "replies": 115, "retuits": 189, "likes": 2236,
+     "dislikes": 0, "handle": "tesla",
+     "tuit": "Tesla Immersive, our multichannel audio upmixer, enables stereo content to be remixed in real time, optimizing the listening experience for our vehicles specifically"},
     },
 ];
     
