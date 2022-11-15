7cd93fcac5e861fdc7fdbccf272aeb415314a744
Rainie Zhao <rainiezhao@Rainies-MacBook-Pro-2.local>
2022-11-14 16:24:44 -0800

    fixed typo

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -16,6 +16,6 @@ export default [
      "replies": 115, "retuits": 189, "likes": 2236,
      "dislikes": 0, "handle": "tesla",
      "tuit": "Tesla Immersive, our multichannel audio upmixer, enables stereo content to be remixed in real time, optimizing the listening experience for our vehicles specifically"},
-    },
+    }
 ];
     
