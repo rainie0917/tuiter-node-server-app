c45aec2cb4220a275bec04de6bec70f315d7c4d7
Rainie Zhao <rainiezhao@Rainies-MacBook-Pro-2.local>
2022-11-15 16:29:47 -0800

    add mongodb

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -12,6 +12,7 @@ const TuitsController = (app) => {
 const createTuit = async (req, res) => {
     const newTuit = req.body;
     // newTuit._id = (new Date()).getTime()+'';
+    newTuit.image = "./nasa.png";
     newTuit.likes = 0;
     newTuit.liked = false;
     newTuit.dislikes = 0;
