35c86eeeb667aa01b46ac86ec91841ea698b4850
rainie0917 <98671263+rainie0917@users.noreply.github.com>
2022-11-18 14:16:06 -0800

    Update tuits-controller.js

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -11,7 +11,6 @@ const TuitsController = (app) => {
 const createTuit = (req, res) => {
     const newTuit = req.body;
     newTuit._id = (new Date()).getTime()+'';
-    newTuit.image = "./nasa.png";
     newTuit.likes = 0;
     newTuit.liked = false;
     newTuit.dislikes = 0;
