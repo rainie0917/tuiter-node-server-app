fc818c77008d36a5777df377722f815ed8593b1b
Rainie Zhao <rainiezhao@Rainies-MacBook-Pro-2.local>
2022-11-18 14:46:29 -0800

    a9

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -12,7 +12,6 @@ const TuitsController = (app) => {
 const createTuit = async (req, res) => {
     const newTuit = req.body;
     // newTuit._id = (new Date()).getTime()+'';
-    newTuit.image = "./nasa.png";
     newTuit.likes = 0;
     newTuit.liked = false;
     newTuit.dislikes = 0;
