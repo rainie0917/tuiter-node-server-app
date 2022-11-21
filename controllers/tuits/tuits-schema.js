e72239c1a97b7e88e2c6902c8323eb398d423599
Rainie Zhao <rainiezhao@Rainies-MacBook-Pro-2.local>
2022-11-21 12:07:00 -0800

    updated tuits-schedma

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
   tuit: String,
-  likes: Number,
+  topic: String,
   liked: Boolean,
+  dislikes: Number,
+  likes: Number,
+  disliked: Boolean,
+  retuits: Number,
+  replies: Number,
+  handle: String,
 }, {collection: 'tuits'});
 export default schema;
 
